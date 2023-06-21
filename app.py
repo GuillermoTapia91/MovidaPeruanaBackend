19207adc60fd99d579d7d0ffb6cd52865bbe2b52
GuillermoTapia91 <guillermo.tapia1991@gmail.com>
2023-06-21 16:22:40 -0500

    creacion de tablas

--- app.py
+++ app.py
@@ -2,8 +2,12 @@ from flask import Flask
 from flask_restful import Api
 from config import conexion
 from flask_migrate import Migrate
+from models.usuario_model import UsuarioModel
+from models.establecimiento_model import EstablecimientoModel
+from models.eventos_model import EventoModel
 from os import environ
 from dotenv import load_dotenv
+
 #from flask_cors import CORS #instalar libreria
 
 load_dotenv()
@@ -12,6 +16,10 @@ app = Flask(__name__)
 api= Api(app)
 
 app.config['SQLALCHEMY_DATABASE_URI']=environ.get('DATABASE_URL')
+
+conexion.init_app(app)
+
+Migrate(app, conexion)
 
 if __name__ == ("__main__"):
     app.run(debug=True)
